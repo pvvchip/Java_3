4ef0e2ed66ddfb79af5ee9c9ff86c7688f274dcd
pvvchip <pvvchip@mail.ru>
2018-09-22 10:57:55 +0300

    lesson 1, point 1, 2

--- com.home.java3/src/Main.java
+++ com.home.java3/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+//      Пункт 1.
+        MyGeneric<Integer> inums = new MyGeneric<>(new Integer[]{10, 20, 30, 40});
+        MyGeneric<String> dnums = new MyGeneric<>(new String[]{"Test1", "Test2", "Test3", "Test4"});
+
+        System.out.println();
+
+        inums.show();
+        inums.chElements(0, 2);
+        inums.show();
+
+        System.out.println();
+
+        dnums.show();
+        dnums.chElements(1, 3);
+        dnums.show();
+
+        System.out.println();
+
+//      Пункт 2.
+        ArrayList<Integer> alInt;
+        ArrayList<String> alDou;
+
+        alInt = inums.arList();
+        System.out.println(Arrays.toString(alInt.toArray()));
+        alDou = dnums.arList();
+        System.out.println(Arrays.toString(alDou.toArray()));
+
+//      Пункт 3.
+
     }
+
 }
---
+++ com.home.java3/src/MyGeneric.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyGeneric<T> {
+    private T[] object;
+
+    public MyGeneric(T[] object) {
+        this.object = object;
+    }
+
+    public void chElements(int i, int i1) {
+        T n = this.object[i1];
+        this.object[i1] = this.object[i];
+        this.object[i] = n;
+    }
+
+    public void show() {
+        System.out.println(Arrays.toString(this.object));
+    }
+
+    public ArrayList<T> arList() {
+        ArrayList<T> al = new ArrayList<>();
+        for (T i:this.object) al.add(i);
+        return al;
+    }
+}
