c673fb580907ab68952cdf5f44077914f1777328
pvvchip <pvvchip@mail.ru>
2018-10-11 10:30:57 +0300

    lesson 6 p: 1, 2, 3

--- com.home.java3/src/lesson_6/Main.java
+++ com.home.java3/src/lesson_6/Main.java
@@ -5,31 +5,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-//    protected static int[] arr = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+    //    protected static int[] arr = {1, 2, 4, 4, 2, 3, 4, 1, 7};
     protected static int[] arr = {2, 3, 4, 6, 7, 8, 9};
-    protected static List<Integer> arr2 = new ArrayList<>();
+    protected static int[] arr2 = {1, 4, 1, 4, 4, 4, 1, 2, 1};
+
+    protected static List<Integer> arrList = new ArrayList<>();
 
 
     protected static List<Integer> operation(int[] i) {
-        System.out.println(Arrays.toString(i));
+        System.out.println("Test1: " + Arrays.toString(i));
         int n = -1;
         for (int j = 0; j < i.length; j++) {
             if (i[j] == 4) n = j;
         }
         if (n != -1) {
             for (int j = n + 1; j < i.length; j++) {
-                arr2.add(arr[j]);
+                arrList.add(arr[j]);
             }
         }
         if (n == -1) {
             System.out.println("Маркера нет");
             throw new RuntimeException();
         }
-        return arr2;
+        return arrList;
+    }
 
+    protected static boolean operation2(int[] arr) {
+        System.out.println("Test2: " + Arrays.toString(arr));
+        for (int i: arr) {
+            if (!(i == 1 || i == 4)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(operation(arr));
+        System.out.println("Test1: " + operation(arr));
+        System.out.println("Test2: " + operation2(arr2));
     }
 }
--- com.home.java3/src/lesson_6/MainTest.java
+++ com.home.java3/src/lesson_6/MainTest.java
@@ -1,5 +1,6 @@
 package lesson_6;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,24 +14,58 @@ public class MainTest {
 
 
     @Test
+    // При одиночном запуске ОК
+    // При запуске всех тестов Failed ?
     public void operationTest() {
         int[] arr = {2, 3, 4, 6, 7, 8, 9};
         List<Integer> arr3 = Arrays.asList(6, 7, 8, 9);
-        Assert.assertArrayEquals(new List[]{arr3}, new List[]{operation(arr)});
+        Assert.assertArrayEquals(new List[]{arr3}, new List[]{Main.operation(arr)});
     }
 
     @Test
     public void operationTest2() {
         int[] arr = {4, 4, 4, 6, 7, 8, 9};
         List<Integer> arr3 = Arrays.asList(6, 7, 8, 9);
-        Assert.assertArrayEquals(new List[]{arr3}, new List[]{operation(arr)});
+        Assert.assertArrayEquals(new List[]{arr3}, new List[]{Main.operation(arr)});
     }
 
     @Test
     public void operationTest3() {
         int[] arr = {2, 3, 0, 6, 7, 3, 9};
         List<Integer> arr3 = Arrays.asList(9);
-        Assert.assertArrayEquals(new List[]{arr3}, new List[]{operation(arr)});
+        Assert.assertArrayEquals(new List[]{arr3}, new List[]{Main.operation(arr)});
     }
 
+    @Test
+    // При одиночном запуске ОК
+    // При запуске всех тестов Failed ?
+    public void operationTest4() {
+        int[] arr = {2, 3, 0, 4, 7, 3, 9};
+        List<Integer> arr3 = Arrays.asList(7, 8, 9);
+        Assert.assertArrayEquals(new List[]{arr3}, new List[]{Main.operation(arr)});
+    }
+
+    @Test
+    public void operation2() {
+        int[] arrTest = {2, 3, 0, 6, 7, 3, 9};
+        Assert.assertTrue("False", Main.operation2(arrTest));
+    }
+
+    @Test
+    public void operation2Test2() {
+        int[] arrTest = {1, 4, 4, 1, 1, 1, 1};
+        Assert.assertTrue("False", Main.operation2(arrTest));
+    }
+
+    @Test
+    public void operation2Test3() {
+        int[] arrTest = {1, 1, 1, 1, 1};
+        Assert.assertTrue("False", Main.operation2(arrTest));
+    }
+
+    @Test
+    public void operation2Test4() {
+        int[] arrTest = {4, 4, 4, 4};
+        Assert.assertTrue("False", Main.operation2(arrTest));
+    }
 }
